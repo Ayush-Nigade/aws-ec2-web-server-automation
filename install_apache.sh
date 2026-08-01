4bccf9e10fdca467169cf0611551f8c73de4f5a4
AYUSH NIGADE <ayushnigade07@gmail.com>
2026-08-02 02:44:45 +0530

    Add files via upload

---
+++ install_apache.sh
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>AWS EC2 Web Server Automation</title>
+<style>
+body{
+font-family:Arial;
+background:#0f172a;
+color:white;
+text-align:center;
+margin-top:100px;
+}
+h1{
+color:#38bdf8;
+}
+</style>
+</head>
+<body>
+
+<h1>AWS EC2 Web Server Automation</h1>
+
+<p>Apache Installed Automatically using EC2 User Data</p>
+
+<p>Project by Ayush Nigade</p>
+
+</body>
+</html>
